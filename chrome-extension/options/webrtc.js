b8a5f0197bc7cefb65aa965e23bfe5eec2f3426a
Dele Olajide <dele@traderlynk.com>
2017-10-31 12:07:41 +0000

    fix permission issue with surface pro

--- chrome-extension/options/webrtc.js
+++ chrome-extension/options/webrtc.js
@@ -1,32 +1,34 @@
 window.addEventListener("load", function()
 {
- 	navigator.webkitGetUserMedia({ audio: true, video: true },
-                function (stream) {
-               
-                    setTimeout(function() 
-                    {
-			    stream.getAudioTracks().forEach(function (track) 
-			    {
-				track.stop();
-			    });
-                    
-                    }, 1000);
+	console.log("Etherlynk is trying to get permissions to use your video/audio devices");
 
-                },
-                function (error) {
-                    alert("To experience the full functionality of TraderLynk Belfry, please connect audio and video devices.");
-                    console.error("Error trying to get the stream:: " + error.message);
-                }
-        );
-        
+	navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(function(stream)
+	{
+		console.log("Etherlynk now has permissions to use your video/audio devices");
+
+		setTimeout(function()
+		{
+			stream.getAudioTracks().forEach(function (track)
+			{
+				console.log("stopping track")
+				track.stop();
+			});
+
+		}, 120000);
+	})
+	.catch(function(err) {
+		alert("To experience the full functionality of Etherlynk, please connect audio and video devices.");
+		console.error("Error trying to get the stream:: " + err.message);
+	});
+
 	navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-	
+
 	function onMIDISuccess(midiAccess) {
 	    console.log('MIDI Access Object', midiAccess);
 	}
 
 	function onMIDIFailure(e) {
 	    console.error("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
-	}	
-       
+	}
+
 });
